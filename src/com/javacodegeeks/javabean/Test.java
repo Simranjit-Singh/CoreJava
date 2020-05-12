65402f1937f3490d45e5cf83ce98a15220d8f603
Simranjit Singh Simranjit Singh <ssimranj1@isimtl.local>
2020-05-12 15:47:22 -0400

    Initial commit

---
+++ src/com/javacodegeeks/javabean/Test.java
@@ -0,0 +1,10 @@
+package com.javacodegeeks.javabean;
+
+public class Test {
+    public static void main(String args[])
+    {
+        Student s = new Student(); // object is created
+        s.setName("JavaCodeGeeks"); // setting value to the object
+        System.out.println(s.getName());
+    }
+}
